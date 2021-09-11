a96a56fc420ea275360640f307cfbdecd56a59b4
OBKoro1 <1677593011@qq.com>
2021-09-11 17:42:42 +0800

    feat: 选择排序

--- src/js/arrary-sort-selection.js
+++ src/js/arrary-sort-selection.js
@@ -1,16 +1,25 @@
-// 选择排序
-// 思路: 找到目标值的下标，与当前遍历元素的下标交换。
+/*
+ * Author       : OBKoro1
+ * Date         : 2021-08-06 00:23:28
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2021-09-11 17:42:06
+ * FilePath     : /js-base/src/js/arrary-sort-selection.js
+ * description  : 选择排序
+ * 思路: 找到目标值的下标，与当前遍历元素的下标交换。
+ * koroFileheader VSCode插件
+ * Copyright (c) 2021 by OBKoro1, All Rights Reserved. 
+ */
 function selectionSort(arr) {
   for (var i = 0; i < arr.length; i++) {
     var indexMin = i // TODO： 当前下标
     // j = i i就是已经排序过的
     for (var j = i; j < arr.length; j++) {
-      // TODO: 寻找目标值 这里是最小值 也可以是其他判断条件
+      //  寻找目标值 这里是最小值 也可以是其他判断条件
       if (arr[indexMin] > arr[j]) {
         indexMin = j
       }
     }
-    // TODO: 下标不同 与当前遍历元素的下标交换。
+    //  下标不同 与当前遍历元素的下标交换。
     if (indexMin !== i) {
       ;[arr[indexMin], arr[i]] = [arr[i], arr[indexMin]]
     }
