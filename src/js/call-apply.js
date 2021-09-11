59e40f28cfd12cde6a4b28eda5e83e05cec5cc38
OBKoro1 <1677593011@qq.com>
2021-09-11 17:44:17 +0800

    feat: 实现call、apply方法

--- src/js/call-apply.js
+++ src/js/call-apply.js
@@ -1,32 +1,43 @@
+/*
+ * Author       : OBKoro1
+ * Date         : 2021-07-29 23:54:01
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2021-09-11 17:43:47
+ * FilePath     : /js-base/src/js/call-apply.js
+ * description  : 实现call、apply方法
+ * koroFileheader VSCode插件
+ * Copyright (c) 2021 by OBKoro1, All Rights Reserved. 
+ */
+
 Function.prototype.myCall = function (context, ...paramsArr) {
-    // TODO: 确定this执行
+    //  确定this执行
     if (context === null || context === undefined) {
        // 指定为 null 和 undefined 的 this 值会自动指向全局对象(浏览器中为window)
         context = window 
     } else {
         context = Object(context) // 值为原始值（数字，字符串，布尔值）的 this 会指向该原始值的实例对象
     }
-    // TODO: 临时储存函数
+    //  临时储存函数
     const specialPrototype = Symbol('特殊属性Symbol')
     context[specialPrototype] = this;
-    let result = context[specialPrototype](...paramsArr); // TODO: 通过隐式绑定执行函数并传递参数 绑定this
+    let result = context[specialPrototype](...paramsArr); //  通过隐式绑定执行函数并传递参数 绑定this
     delete context[specialPrototype]; // 删除上下文对象的属性
     return result; // 返回函数执行结果
 };
 
-// TODO: 只改变传参
+//  只改变传参
 Function.prototype.myApply = function (context, paramsArr) {
-    // TODO: 确定this执行
+    //  确定this执行
     if (context === null || context === undefined) {
         // 指定为 null 和 undefined 的 this 值会自动指向全局对象(浏览器中为window)
          context = window 
      } else {
          context = Object(context) // 值为原始值（数字，字符串，布尔值）的 this 会指向该原始值的实例对象
      }
-     // TODO: 临时储存函数
+     //  临时储存函数
      const specialPrototype = Symbol('特殊属性Symbol')
      context[specialPrototype] = this;
-     let result = context[specialPrototype](...paramsArr); // TODO: 通过隐式绑定执行函数并传递参数 绑定this
+     let result = context[specialPrototype](...paramsArr); //  通过隐式绑定执行函数并传递参数 绑定this
      delete context[specialPrototype]; // 删除上下文对象的属性
      return result; // 返回函数执行结果
 };
