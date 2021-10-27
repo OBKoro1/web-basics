baf1fc18f6389c4f80a35bfc544e495281a880a3
OBKoro1 <1677593011@qq.com>
2021-10-27 22:36:55 +0800

    feat: 二面异步任务 控制并发数

--- interview/kuaishou.js
+++ interview/kuaishou.js
@@ -2,7 +2,7 @@
  * Author       : OBKoro1
  * Date         : 2021-10-15 14:56:02
  * LastEditors  : OBKoro1
- * LastEditTime : 2021-10-22 15:37:16
+ * LastEditTime : 2021-10-27 22:21:08
  * FilePath     : /js-base/interview/kuaishou.js
  * description  : 快手一面
  * koroFileheader VSCode插件
@@ -90,59 +90,7 @@
 
 // TODO: 这个感觉蛮麻烦的
 
-/**
- * 缓存异步接口
- * - 第一次请求缓存接口的时候，和调用原异步接口效果一样
- * - 缓存接口根据入参缓存原异步接口返回值
- * - 有缓存值的时候，马上返回缓存值，并发起请求更新缓存值
- * - 对于同样的入参，缓存接口同一时刻，最多只会发起一个请求
- * @param fn 原异步接口
- * @returns 缓存接口
- */
-function cacheApi(...args) {
-    // 补充代码实现
-    let map = {}
-    let list = []
-    let fn = args[0]
-    return async function help(key) {
-        // 同时只有一个接口，判断锁
-        if (map[key] && map[key].lock === true) {
-            // 接口已经有过值了 直接返回接口
-            if (map[key].data) {
-                return map[key].data
-            } else {
-                // 接口正在进行第一次请求
-               return await map[key].promise
-            }
-        }
-        // 初始化
-        if (!map[key]) {
-            map[key] = {}
-            // 缓存同一个promise 同一个参数等待他完成
-            map[key].promise = fn(key)
-            map[key].lock = true
-            map[key].data = null
-        }
 
-        // 返回缓存值
-        if (map[key].data) {
-            map[key].lock = true
-            map[key].promise = fn(key)
-            // 下次异步更改数据
-            map[key].promise.then(res => {
-                map[key].data = res
-                map[key].lock = false
-            })
-            return map[key].data
-        } else {
-            // 第一次请求
-            let res = await map[key].promise
-            map[key].data = res
-            map[key].lock = false
-            return res
-        }
-    }
-}
 
 /**
  * mock api
@@ -180,3 +128,78 @@ const cachedApi = cacheApi(mockApi);
     );
     // 马上输出 [ { req: "a", id: 2 }, { req: "b", id: 3 }, { req: "a", id: 2 } ]
 })();
+
+
+
+// 二面
+
+// 异步任务，控制并发数目
+// 代码地址：src/scene/task-concurrent.js
+
+
+
+
+
+
+
+// 答案慎看
+
+
+
+
+
+
+
+/**
+ * 缓存异步接口
+ * - 第一次请求缓存接口的时候，和调用原异步接口效果一样
+ * - 缓存接口根据入参缓存原异步接口返回值
+ * - 有缓存值的时候，马上返回缓存值，并发起请求更新缓存值
+ * - 对于同样的入参，缓存接口同一时刻，最多只会发起一个请求
+ * @param fn 原异步接口
+ * @returns 缓存接口
+ */
+//  function cacheApi(...args) {
+//     // 补充代码实现
+//     let map = {}
+//     let list = []
+//     let fn = args[0]
+//     return async function help(key) {
+//         // 同时只有一个接口，判断锁
+//         if (map[key] && map[key].lock === true) {
+//             // 接口已经有过值了 直接返回接口
+//             if (map[key].data) {
+//                 return map[key].data
+//             } else {
+//                 // 接口正在进行第一次请求
+//                return await map[key].promise
+//             }
+//         }
+//         // 初始化
+//         if (!map[key]) {
+//             map[key] = {}
+//             // 缓存同一个promise 同一个参数等待他完成
+//             map[key].promise = fn(key)
+//             map[key].lock = true
+//             map[key].data = null
+//         }
+
+//         // 返回缓存值
+//         if (map[key].data) {
+//             map[key].lock = true
+//             map[key].promise = fn(key)
+//             // 下次异步更改数据
+//             map[key].promise.then(res => {
+//                 map[key].data = res
+//                 map[key].lock = false
+//             })
+//             return map[key].data
+//         } else {
+//             // 第一次请求
+//             let res = await map[key].promise
+//             map[key].data = res
+//             map[key].lock = false
+//             return res
+//         }
+//     }
+// }
