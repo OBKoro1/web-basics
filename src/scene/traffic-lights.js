9af24712e6090445c3f58315cf48482188a4f7a6
OBKoro1 <1677593011@qq.com>
2021-10-28 18:30:03 +0800

    feat: 红绿灯算法

--- src/scene/traffic-lights.js
+++ src/scene/traffic-lights.js
@@ -2,7 +2,7 @@
  * Author       : OBKoro1
  * Date         : 2021-10-13 14:45:44
  * LastEditors  : OBKoro1
- * LastEditTime : 2021-10-13 17:43:58
+ * LastEditTime : 2021-10-28 18:29:25
  * FilePath     : /js-base/src/scene/traffic-lights.js
  * description  : 红绿灯算法
  * koroFileheader VSCode插件
@@ -22,7 +22,9 @@ async function setColor(i, color) {
     console.log('设置颜色', color)
     await timePromise(i)
 }
+
 async function run() {
+    // 这里用了while循环 递归也可以
     while (true) {
         await setColor(3000, "red");
         await setColor(2000, "green");
