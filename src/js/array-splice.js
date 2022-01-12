f730f66cce8da73a23770c19e45d190eef650629
OBKoro1 <1677593011@qq.com>
2022-01-13 00:55:10 +0800

    feat: update

--- src/js/array-splice.js
+++ src/js/array-splice.js
@@ -2,7 +2,7 @@
  * Author       : OBKoro1
  * Date         : 2021-09-22 13:41:03
  * LastEditors  : OBKoro1
- * LastEditTime : 2021-09-22 13:41:07
+ * LastEditTime : 2022-01-12 15:59:23
  * FilePath     : /js-base/src/js/array-splice.js
  * description  : 实现数组的splice
  * koroFileheader VSCode插件
@@ -21,11 +21,10 @@ Array.prototype._splice = function (start, deleteCount, ...addList) {
   if (typeof deleteCount === 'undefined') {
     deleteCount = this.length - start
   }
-
+  // 删除
   const removeList = this.slice(start, start + deleteCount)
-
   const right = this.slice(start + deleteCount)
-
+  // 添加
   let addIndex = start
   addList.concat(right).forEach((item) => {
     this[addIndex] = item
