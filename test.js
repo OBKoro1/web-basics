30ab09dfb574e4eaba62933184ee241ddb977500
OBKoro1 <1677593011@qq.com>
2022-01-16 18:23:57 +0800

    feat: update

--- test.js
+++ test.js
@@ -2,9 +2,9 @@
  * Author       : OBKoro1
  * Date         : 2022-01-12 16:45:42
  * LastEditors  : OBKoro1
- * LastEditTime : 2022-01-12 17:05:21
+ * LastEditTime : 2022-01-16 15:48:43
  * FilePath     : /js-base/test.js
- * description  : B站面试 主站直播中心-营收组
+ * description  :
  * koroFileheader VSCode插件
  * Copyright (c) 2022 by OBKoro1, All Rights Reserved.
  */
