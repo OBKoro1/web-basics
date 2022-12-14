2ba5b99e8c4cab9c8496e1e7c827a51470aa6e2a
OBKoro1 <1677593011@qq.com>
2022-12-14 19:58:45 +0800

    feat: dataTotree

---
+++ src/scene/arrToTree/index.js
@@ -0,0 +1,47 @@
+// 大同小异 此题留给诸位
+const arr = [{
+  id: 2,
+  name: '部门B',
+  parentId: 0,
+},
+{
+  id: 3,
+  name: '部门C',
+  parentId: 1,
+},
+{
+  id: 1,
+  name: '部门A',
+  parentId: 2,
+},
+{
+  id: 4,
+  name: '部门D',
+  parentId: 1,
+},
+{
+  id: 5,
+  name: '部门E',
+  parentId: 2,
+},
+{
+  id: 6,
+  name: '部门F',
+  parentId: 3,
+},
+{
+  id: 7,
+  name: '部门G',
+  parentId: 2,
+},
+{
+  id: 8,
+  name: '部门H',
+  parentId: 4,
+},
+]
+
+function toTree(arr) {
+
+}
+console.log('toTree', toTree(arr))
